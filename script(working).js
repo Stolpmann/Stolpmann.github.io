d77bd25928a8a477b305eb08115bce8b587aa2fb
Stolpmann <evan.stolpmann@hotmail.com>
2021-03-23 16:24:01 -0400

    style update

---
+++ script(working).js
@@ -0,0 +1,24 @@
+// is a comment
+
+/*
+document.getElementById("filter-Frontend").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("filter-Frontend").style.color = "white";
+}
+*/
+
+/* Project NAVIGATION BAR */
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("projectnav");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) {
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
