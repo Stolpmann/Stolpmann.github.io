6fdb1962300ce4ea018c3f8f3e842557fab06b64
Stolpmann <evan.stolpmann@hotmail.com>
2021-04-22 19:12:14 -0400

    d

--- projects.js
+++ projects.js
@@ -1,8 +1,1 @@
-function myFunction() {
-  var x = document.getElementById("Project1");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
+
