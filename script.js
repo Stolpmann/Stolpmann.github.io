1ab5744b7d64fbd7f5e2c56aabc2e6eb70b44f8b
Stolpmann <evan.stolpmann@hotmail.com>
2021-04-22 19:06:07 -0400

    Update

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ for (var i = 0; i < btns.length; i++) {
   this.className += " active";
   });
 }
+
+
+function myFunction() {
+  var x = document.getElementById("Project1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
