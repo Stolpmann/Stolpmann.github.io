4ea3238fe6b4f5db7a14b4aa9cf44ac5f271ec43
Stolpmann <evan.stolpmann@hotmail.com>
2021-04-22 16:34:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ for (var i = 0; i < btns.length; i++) {
 }
 
 function myFunction() {
-  var x = document.getElementById("myDIV");
+  var x = document.getElementById("Project1");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
