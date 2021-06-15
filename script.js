74af58f424d297d8a14f2ae05623aba87e09da2c
Stolpmann <evan.stolpmann@hotmail.com>
2021-06-15 14:09:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,15 @@
 // Add active class to the current button (highlight it)
 var header = document.getElementById("projectnav");
 var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  if (current.length > 0) {
-    current[0].className = current[0].className.replace(" active", "");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+    });
   }
-  this.className += " active";
-  });
-}
 
-var all = document.getElementById("filter-all").className;
-console.log(all);
+var allprojects = document.getElementById("filter-all").className;
+console.log(allprojects);
